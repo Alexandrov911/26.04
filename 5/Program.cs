b09e7c62b4421efa08174a638ce5f262416d4ea3
Alexandrov911 <sashok17062002@mail.ru>
2021-04-26 14:15:33 +0300

    2

---
+++ 5/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" введите число x ");
+            int x = Convert.ToInt32(Console.ReadLine());
+            double s = 0;
+            for (int i = 1; i <= 11; i=i+2)
+            {
+                s = s + Math.Pow(x,2) / i;
+            }
+            Console.WriteLine(s);
+            Console.ReadKey();
+        }
+    }
+}
