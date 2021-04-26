0f3dbbba4a3831ace15f2a2b603628966e40124c
Alexandrov911 <sashok17062002@mail.ru>
2021-04-26 14:02:12 +0300

    26.04

---
+++ 26.04/3/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" введите число a ");
+            int a = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine(" введите число a ");
+            int b = Convert.ToInt32(Console.ReadLine());
+            int s = 0;
+            for (int i = a; i <= b; i++)
+            {
+                s = s + i * i;
+            }
+            Console.WriteLine(s);
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 26.04/4/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" введите число n ");
+            int n = Convert.ToInt32(Console.ReadLine());
+            double s = 0;
+            for (int i = 1; i <= n; i++)
+            {
+                s = s+1.0/i;
+            }
+            Console.WriteLine(s);
+            Console.ReadKey();
+        }
+    }
+}
